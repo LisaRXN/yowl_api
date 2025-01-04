bad04c198d2fb81c13d65ea03e394013cf6c857e
lisa erksn <lisa.eriksen@epitech.eu>
2025-01-04 09:43:59 +0100

    update Cors Origin for frontend server

--- src/index.js
+++ src/index.js
@@ -4,34 +4,33 @@ const port = 3000;
 const cors = require("cors");
 const session = require("express-session");
 const indexRoutes = require("./routes/index.routes.js");
-const passport = require('../src/controllers/passport-config.js')
-require('dotenv').config(); 
+const passport = require("../src/controllers/passport-config.js");
+require("dotenv").config();
 
-
-//Autorise l'accès' dossier public (pour les images)
-app.use(express.static('public'));
+//Autorise l'accès dossier public (pour les images)
+app.use(express.static("public"));
 
 //GoogleAuth
-app.use(session({
-  secret: 'secret',  
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 //middleware
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
-app.use(express.json({ limit: '10mb' }));
-
-// app.use(session({
-//     secret: 'ton_secret',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: false }  // En mode développement, secure: false (mettre true pour HTTPS)
-// }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://yowl-front-j95pm19om-lisa-eriksens-projects.vercel.app",
+    ],
+  })
+);
+app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
