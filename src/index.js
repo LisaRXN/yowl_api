1b75f83ece9d5f29c1b1d7aaaa121c258ffec069
lisa erksn <lisa.eriksen@epitech.eu>
2025-01-05 12:00:30 +0100

    fix Cors headers

--- src/index.js
+++ src/index.js
@@ -16,22 +16,35 @@ app.use(
     secret: "secret",
     resave: false,
     saveUninitialized: true,
+    sameSite: 'none'
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-//middleware
+//Cors
 app.use(cors());
+const allowedOrigins = [
+  'https://yowl-front-rho.vercel.app',
+  'https://yowl-api-lisa-eriksens-projects.vercel.app',
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use(express.json({ limit: "10mb" }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Yowl Api listening on port ${port}`);
 });
 
 // Init Routes
