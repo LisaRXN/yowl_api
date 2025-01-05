2777186e0480f12389a6995d5136b1f3383f0d80
lisa erksn <lisa.eriksen@epitech.eu>
2025-01-05 12:55:36 +0100

    fix Cors

--- src/index.js
+++ src/index.js
@@ -24,13 +24,6 @@ app.use(passport.session());
 
 //Cors
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://yowl-front-rho.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 app.use(express.json({ limit: "10mb" }));
 
